53012f18d05cdb22557597bbd11db76d268b9f8a
anantjs1 <smspanant@gmail.com>
2017-01-29 12:46:38 +0530

    Added comments to main module js

--- MyResumeApp/myResumeApp.js
+++ MyResumeApp/myResumeApp.js
@@ -1,4 +1,5 @@
-﻿var myResumeApp = angular.module("myResumeApp", ["app.routeConfig","ngSanitize", "app.customFilter", "app.myCareer", "app.home", "app.achievements", "app.hobbies"]);
+﻿//This is the main module app
+var myResumeApp = angular.module("myResumeApp", ["app.routeConfig", "ngSanitize", "app.customFilter", "app.myCareer", "app.home", "app.achievements", "app.hobbies"]);
 myResumeApp.service("globalConstants", globalConstants);
 myResumeApp.controller("myResumeAppController", myResumeAppController); highChartDirective
 myResumeApp.directive("hcPieChart", highChartDirective);
