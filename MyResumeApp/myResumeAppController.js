4186a2172f0b95f9a8f8024290b1f847618d067b
anantjs1 <smspanant@gmail.com>
2017-02-01 22:50:09 +0530

    Changes for main menu select styling

--- MyResumeApp/myResumeAppController.js
+++ MyResumeApp/myResumeAppController.js
@@ -1,6 +1,11 @@
 ﻿var myResumeAppController = function ($scope) {
     var init = function () {
         $scope.headerNavText = globalConfig.strings.Home;
+        //$scope.mainMenuIndex = 0;
     };
     init();
+    $scope.mainMenuClick = function (menuText,menuIndex) {
+        $scope.headerNavText = menuText;
+        $scope.mainMenuIndex = menuIndex;
+    }
 };
